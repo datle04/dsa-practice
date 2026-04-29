d2e57aab0ab26ea6c64a569d2be855d94634c823
unknown <ldat0909@gmail.com>
2026-04-29 19:05:03 +0700

    daily: solved leetcode 238

---
+++ patterns/01_hash-map/medium/238_product-of-array-except-self.ts
@@ -0,0 +1,17 @@
+function productExceptSelf(nums: number[]): number[] {
+    let res = new Array(nums.length).fill(0);
+    
+    let prefix = 1;
+    for (let i = 0; i < nums.length; i++){
+        res[i] = prefix;
+        prefix *= nums[i];
+    };
+
+    let suffix = 1;
+    for (let i = nums.length - 1; i >= 0; i--){
+        res[i] = suffix * res[i];
+        suffix *= nums[i];
+    }
+
+    return res;
+};
