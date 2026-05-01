316fae87c967bd267c3687a46cf6fcf3ca98d92a
unknown <ldat0909@gmail.com>
2026-05-01 16:14:09 +0700

    daily: solved leetcode 283

---
+++ patterns/02_two-pointers/easy/283_move-zeroes.ts
@@ -0,0 +1,15 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function moveZeroes(nums: number[]): void {
+    let left = 0;
+    let right = 0;
+
+    while (right < nums.length){
+        if(nums[right] !== 0){
+            [nums[left], nums[right]] = [nums[right], nums[left]]
+            left++;
+        }
+        right++;
+    }
+};
