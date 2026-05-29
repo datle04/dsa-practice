1037a122f109d9fba822d32dac64cf4cca2a6901
Dat <ldat0909@gmail.com>
2026-05-29 12:05:14 +0700

    daily: solved leetcode 19

---
+++ patterns/06_linked_list/medium/19_remove-nth-node-from-end-of-list.ts
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
+    let dummy = new ListNode(0, head);
+    let fast = dummy;
+    let slow = dummy;
+
+    for(let i = 0; i <= n; i++){
+        fast = fast.next;
+    }
+
+    while(fast){
+        fast = fast.next;
+        slow = slow.next;
+    }
+
+    slow.next = slow.next.next;
+
+    return dummy.next;
+};
