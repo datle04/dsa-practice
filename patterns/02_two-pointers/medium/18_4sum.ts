e02e4994f343390d5a64a8ad465fff44118a609e
Dat <ldat0909@gmail.com>
2026-06-03 16:03:42 +0700

    daily: solved leetcode 18

---
+++ patterns/02_two-pointers/medium/18_4sum.ts
@@ -0,0 +1,31 @@
+function fourSum(nums: number[], target: number): number[][] {
+    nums.sort((a, b) => a - b);
+    let res = [];
+
+    for(let i = 0; i < nums.length; i++){
+        if(i > 0 && nums[i] === nums[i - 1]) continue;
+
+        for(let j = i + 1; j < nums.length; j++){
+            if(j > i + 1 && nums[j] === nums[j - 1]) continue;
+            let completion = target - nums[i] - nums[j];
+            let left = j + 1;
+            let right = nums.length - 1;
+
+            while(left < right){
+                if(nums[left] + nums[right] === completion){
+                    res.push([nums[i], nums[j], nums[left], nums[right]]);
+                    left++;
+                    right--;
+                    while(left < right && nums[left] === nums[left - 1]) left++;
+                    while(left < right && nums[right] === nums[right + 1]) right--;
+                } else if (nums[left] + nums[right] > completion){
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+    }
+
+    return res;
+};
