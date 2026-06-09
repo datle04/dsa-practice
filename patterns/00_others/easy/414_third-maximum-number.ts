544386beeca6520706357b5b460b2eb3ad31446b
Dat <ldat0909@gmail.com>
2026-06-09 15:45:18 +0700

    daily: solved leetcode 414

---
+++ patterns/00_others/easy/414_third-maximum-number.ts
@@ -0,0 +1,21 @@
+function thirdMax(nums: number[]): number {
+    let first = -Infinity;
+    let second = -Infinity;
+    let third = -Infinity;
+
+    for(const num of nums){
+        if(num === first || num === second || num === third) continue;
+        if(num > first){
+            third = second;
+            second = first;
+            first = num;
+        } else if (num > second){
+            third = second;
+            second = num;
+        } else if (num > third){
+            third = num;
+        }
+    }
+
+    return third === -Infinity ? first : third;
+};
